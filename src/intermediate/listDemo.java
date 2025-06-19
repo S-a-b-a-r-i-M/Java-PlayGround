a165e8f6083650e60df5ef6c777b4447ba2dcff3
sabari-23133 <sabarinithi2002@gmail.com>
2025-06-19 15:56:30 +0530

    changes

---
+++ src/intermediate/listDemo.java
@@ -0,0 +1,12 @@
+package intermediate;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class listDemo {
+
+    public static void main(String[] args) {
+        List<Integer> immutableList = Collections.singletonList(1);
+    }
+}
